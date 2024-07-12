8cb9004a4313cad94bfbda0d129398d109049a02
Arion <arionking@hotmail.com>
2024-07-12 12:05:27 +0100

    Added reload page function to .j file and working on click button

---
+++ index.js
@@ -0,0 +1,6 @@
+function reloadPage() {
+    window.location.reload()
+}
+// window is the global window OBJECT
+// location is a PROPERTY of the window object 
+// reload is a METHOD of the location object or property
