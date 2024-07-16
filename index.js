dcc132543117194b74106a39675236e7153ad564
Arion <arionking@hotmail.com>
2024-07-16 10:51:59 +0100

    added html ONLY for the slideshow

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function reloadHomePage() {
     window.location.href = 'index.html';
 }
-// window is the global window OBJECT
-// location is a PROPERTY of the window object 
-// reload is a METHOD of the location object or property
+
+// slideshow code // 
